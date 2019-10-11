1336ecd42d879782ecf6f10bc94a92f84ecf03e9
splele <56450761+splele@users.noreply.github.com>
2019-10-11 23:09:12 +0530

    Add files via upload

---
+++ 4th.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main()
+
+{
+    printf("Enter Your First Name:");
+    char name1[10];
+    scanf("%s",&name1);
+    printf("Enter Your Last Name:");
+    char name2[10];
+    scanf("%s",&name2);
+    printf("\n---------------------------------------------");
+    printf("\nYour Name Is: %s %s",name1,name2);
+    printf("\n---------------------------------------------");
+
+
+
+
+
+
+}
